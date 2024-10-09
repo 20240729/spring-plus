8cbb01d1c7828d3dc7583d5de4027e12b07fd964
20240729 <yoohyeonsik@gmail.com>
2024-10-09 17:23:27 +0900

    1-9 해결 : SecurityConfig 추가, JwtSecurityFilter 추가, JwtAuthenticationToken 추가 후 기존 코드를 사양에 맞게 수정

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -1,16 +1,14 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.user.dto.request.UserChangeRequest;
 import org.example.expert.domain.user.dto.response.UserNicknameResponse;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,13 +23,13 @@ public class UserController {
 
     // 여기에 닉네임 변경 기능 추가?
     @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser, @RequestBody UserChangeRequest userChangeRequest) {
+    public void changePassword(@AuthenticationPrincipal AuthUser authUser, @RequestBody UserChangeRequest userChangeRequest) {
         userService.changePassword(authUser.getId(), userChangeRequest);
     }
 
     // 로그인된 유저 닉네임을 가져오는 url
     @GetMapping("/users/nickname")
-    public ResponseEntity<UserNicknameResponse> getUserNickName(@Auth AuthUser authUser) {
+    public ResponseEntity<UserNicknameResponse> getUserNickName(@AuthenticationPrincipal AuthUser authUser) {
         return ResponseEntity.ok(userService.getUserNickName(authUser.getId()));
     }
 }
